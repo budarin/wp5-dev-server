fa9cde9d98d34189a97bc250f2a3122c96f5eba9
vadim_budarin <budarin-vv@yandex.ru>
2020-12-05 21:36:21 +0300

    fix: refactor home as module

--- src/home.js
+++ src/home.js
@@ -1,1 +1,3 @@
-document.getElementById('app').innerText = 'Hello World!';
+const func = () => (document.getElementById('app').innerText = 'Hello World!');
+
+export default func;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
     import(/* webpackChunkName: "Home" */ './home')
-        .then((src) => console.log('Home.js is loaded.', src))
+        .then((src) => {
+            console.log('Home.js is loaded.');
+            src.default();
+        })
         .catch((err) => console.log('Error loading Home.js', err));
 });
